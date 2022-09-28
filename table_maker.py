4c99e4ea98e685c8a9beefdd8ee4aaa9537c22e0
YMertOvguer <112813253+YMertOvguer@users.noreply.github.com>
2022-09-28 22:00:25 +0300

    Add files via upload

---
+++ table_maker.py
@@ -0,0 +1,43 @@
+# adding libs
+import numpy as np
+import random
+
+# setup
+table = np.zeros((7, 7), dtype=str)
+listed_event = []
+plan = []
+counter = 0
+
+# presentation
+print("""
+-------------------------------------------------
+
+enter you event name
+            at least whatever you want...
+
+press 'q' and hit enter button to quit
+
+-------------------------------------------------
+""")
+
+# getting lectures
+while True:
+    event = input("Enter lecture name: ")
+    event = str(event)
+    if event == "q":
+        break
+    else:
+        listed_event.append(event)
+
+# creating lecture plan
+
+N = len(event)
+while True:
+    a = random.randint(0, N-1)
+    plan.append(event[a])
+    counter += 1
+    if counter == 49:
+        break
+event_plan = np.array(plan)
+event_plan = event_plan.reshape(7, 7)
+print(event_plan)
